0648a8b06e75dab2e75eb18d1a17dc31b33f837c
May <mayyuhein@gmail.com>
2021-06-23 23:30:01 -0400

    Added utility functions

--- server/api/index.js
+++ server/api/index.js
@@ -1,30 +1,45 @@
-const router = require('express').Router()
-const { characterCount } = require('../utility');
+const router = require("express").Router();
+const {
+  countCharacters,
+  countWords,
+  countParagraphs,
+  countUniqueBigrams,
+} = require("../utility");
 
 // Character Count
 router.get("/characterCount", (req, res, next) => {
   if (req.body) {
     try {
       let { text } = req.body;
-      let charCount = characterCount(text);
+      let charCount = countCharacters(text);
       res.send(`Character Count: ${charCount}`);
     } catch (error) {
       next(error);
     }
   } else {
-    res.send("Please provide text in the body.")
+    res.send("Please provide text in the body.");
   }
 });
 
 // Word Count
 router.get("/wordCount", (req, res, next) => {
-  res.send("Hi, I'm wordCount")
+  if (req.body) {
+    try {
+      let { text } = req.body;
+      let wordCount = wordCount(text);
+      res.send(`Character Count: ${charCount}`);
+    } catch (error) {
+      next(error);
+    }
+  } else {
+    res.send("Please provide text in the body.");
+  }
 });
 
 router.use((req, res, next) => {
-  const error = new Error('Not Found')
-  error.status = 404
-  next(error)
-})
+  const error = new Error("Not Found");
+  error.status = 404;
+  next(error);
+});
 
 module.exports = router;
--- server/utility/index.js
+++ server/utility/index.js
@@ -1,16 +1,36 @@
 // Character Count
-const characterCount = (text) => {
-  let count = 0;
-  for (let char in text) {
-    if (text[char] !== ' ') {
-      count++;
-    }
-  }
-  return count;
-}
+const countCharacters = (str) => str.length;
 
-module.exports = {
-  characterCount
+const isBlankString = (str) => str.trim().length == 0;
+
+const getWords = (str) => {
+  return str
+    .split(" ")
+    .map((substr) => substr.trim())
+    .filter((substr) => substr.length > 0);
 };
 
+// Word Count
+const countWords = (str) => getWords(str).length;
 
+// Paragraph Count
+const countParagraphs = (str) => {
+  return str.split("\n").filter((substr) => !isBlankString(substr)).length;
+};
+
+// Unique Bigrams
+const countUniqueBigrams = (str) => {
+  const words = getWords(str);
+  let bigrams = new Set();
+  for (let i = 0; i + 1 < words.length; i++) {
+    bigrams.add(words[i] + words[i + 1]);
+  }
+  return bigrams.size;
+};
+
+module.exports = {
+  countCharacters,
+  countWords,
+  countParagraphs,
+  countUniqueBigrams,
+};
